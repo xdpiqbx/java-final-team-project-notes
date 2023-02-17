d0b51800df120395be7501aa0cbcb8c13ba8f66f
xdpiqbx <xdpiqbx@gmail.com>
2023-02-17 11:40:26 +0200

    first commit

---
+++ src/main/java/ua/goit/notes_proj/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ua.goit.notes_proj;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NotesApplication.class);
+	}
+
+}
