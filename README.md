859c9cfa88524aacd20c8d5425f3aeee16900482
xdpiqbx <xdpiqbx@gmail.com>
2023-02-28 15:24:48 +0200

    test commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # java-final-team-project-notes
+
+## BeesTeam
--- src/main/java/ua/goit/notes_proj/ServletInitializer.java
+++ src/main/java/ua/goit/notes_proj/ServletInitializer.java
@@ -2,13 +2,10 @@ package ua.goit.notes_proj;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
 public class ServletInitializer extends SpringBootServletInitializer {
-
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(NotesApplication.class);
 
 	}
-
 }
