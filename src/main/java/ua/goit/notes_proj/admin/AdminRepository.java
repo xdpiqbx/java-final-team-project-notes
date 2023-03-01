74ebfd995a8a7a91b4df167325cc851766d67732
xdpiqbx <xdpiqbx@gmail.com>
2023-03-01 21:42:49 +0200

    hackeremove AdminRepo

--- src/main/java/ua/goit/notes_proj/admin/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package ua.goit.notes_proj.admin;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ua.goit.notes_proj.author.Author;
-
-public interface AdminRepository extends JpaRepository<Author, Long> {
-}
